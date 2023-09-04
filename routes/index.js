393b31231a46e4a3de273ce69347b89c6094629a
yeojin <yeojin7010+L@gmail.com>
2023-09-04 21:46:37 +0900

    주석 삭제

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,10 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-//var path = require('path');
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   //res.sendFile(path.join(__dirname, '/../CAUtion-Web-Front-End/public/index.html'));
 
-  res.send("CAUtion Web Page")
+  res.send("CAUtion Web Page");
 });
 
 module.exports = router;
